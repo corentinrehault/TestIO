3fca69da28b059ea7848359ff1a777818db5587f
corentinrehault <corentin.rehault@gmail.com>
2014-11-25 12:30:47 +0100

    Complet

--- TestIO/src/fr/Container.java
+++ TestIO/src/fr/Container.java
@@ -13,28 +13,26 @@ import java.util.List;
  */
 public class Container {
 
-	static List<String> listDeMot = new ArrayList<String>();
-	static List<String> listVer = new ArrayList<String>();
-	static HashMap<String, Integer> listPremiereLettre = new HashMap<String, Integer>();
-	static HashMap<Integer, Integer> listNbreLettres = new HashMap<Integer, Integer>();
+	public static List<String> listDeMot = new ArrayList<String>();
+	public static List<String> listVer = new ArrayList<String>();
+	public static HashMap<String, Integer> listPremiereLettre = new HashMap<String, Integer>();
+	public static HashMap<Integer, Integer> listNbreLettres = new HashMap<Integer, Integer>();
 
 	static void buildGeneralList(String mot) {
 		listDeMot.add(mot);
 	}
-	
+
 	static void builEndVerList(String mot) {
 		listVer.add(mot);
 	}
-	
+
 	static void buildPremiereLettreList(String premiereLettre, Integer nombreDeMots) {
 		listPremiereLettre.put(premiereLettre, nombreDeMots);
 	}
-	
-	
-	
+
 	static void buildNbreLettresList(Integer nombreDeLettres, Integer nombreDeMots) {
 		listNbreLettres.put(nombreDeLettres, nombreDeMots);
 	}
-	
+
 
 }
--- TestIO/src/fr/ScannerTexte.java
+++ TestIO/src/fr/ScannerTexte.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package fr;
 
 import java.io.IOException;
@@ -14,10 +15,18 @@ import java.util.Scanner;
  */
 public class ScannerTexte {
 
-	public static void scanTexte(String monUrl) throws MalformedURLException, IOException {
+	public static String titre = new String();
 
-		Scanner scan = new Scanner(new URL(monUrl).openStream());
-		String titre = new String("<title>");
+	/**
+	 * @param monUrl
+	 * @param charSet
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public static void scanTexte(String monUrl, String charSet) throws MalformedURLException, IOException {
+
+		Scanner scan = new Scanner(new URL(monUrl).openStream(), charSet);
+
 		int nbreDeMotsa = 0;
 		int nbreDeMotsb = 0;
 		int nbreDeMotsc = 0;
@@ -45,6 +54,32 @@ public class ScannerTexte {
 		int nbreDeMotsy = 0;
 		int nbreDeMotsz = 0;
 
+		int nbreDeLettres1 = 0;
+		int nbreDeLettres2 = 0;
+		int nbreDeLettres3 = 0;
+		int nbreDeLettres4 = 0;
+		int nbreDeLettres5 = 0;
+		int nbreDeLettres6 = 0;
+		int nbreDeLettres7 = 0;
+		int nbreDeLettres8 = 0;
+		int nbreDeLettres9 = 0;
+		int nbreDeLettres10 = 0;
+		int nbreDeLettres11 = 0;
+		int nbreDeLettres12 = 0;
+		int nbreDeLettres13 = 0;
+		int nbreDeLettres14 = 0;
+		int nbreDeLettres15 = 0;
+		int nbreDeLettres16 = 0;
+		int nbreDeLettres17 = 0;
+		int nbreDeLettres18 = 0;
+		int nbreDeLettres19 = 0;
+		int nbreDeLettres20 = 0;
+		int nbreDeLettres21 = 0;
+		int nbreDeLettres22 = 0;
+		int nbreDeLettres23 = 0;
+		int nbreDeLettres24 = 0;
+		int nbreDeLettres25 = 0;
+
 		//Tableau alphabétique
 		char letr = 'a';
 		char alphabet[] = new char[26]; 
@@ -64,139 +99,245 @@ public class ScannerTexte {
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			if (line.contains("<title>")) {
-				//				System.out.println(line.length());
+				titre = line.toString();
+				titre = titre.replace("<title>", "");
+				titre = titre.replace("</title>", "");
+				titre = titre.replace(" ", "_");
 			}
 			if (!line.contains("<")) {
 				//				System.err.println(line); 
 				Container.buildGeneralList(line);
+
+				//Méthode par switch pour l'alphabet
+
+				switch (line.charAt(0)) {
+				case 'a': case 'A': case 'à': case 'â':
+					nbreDeMotsa++;
+					Container.buildPremiereLettreList("a", nbreDeMotsa);
+					break;
+				case 'b': case 'B':
+					nbreDeMotsb++;
+					Container.buildPremiereLettreList("b", nbreDeMotsb);
+					break;
+				case 'c': case 'C': case 'ç':
+					nbreDeMotsc++;
+					Container.buildPremiereLettreList("c", nbreDeMotsc);
+					break;
+				case 'd': case 'D':
+					nbreDeMotsd++;
+					Container.buildPremiereLettreList("d", nbreDeMotsd);
+					break;
+				case 'e': case 'E': case 'é': case 'è': case 'ê':
+					nbreDeMotse++;
+					Container.buildPremiereLettreList("e", nbreDeMotse);
+					break;
+				case 'f': case 'F':
+					nbreDeMotsf++;
+					Container.buildPremiereLettreList("f", nbreDeMotsf);
+					break;
+				case 'g': case 'G':
+					nbreDeMotsg++;
+					Container.buildPremiereLettreList("g", nbreDeMotsg);
+					break;
+				case 'h': case 'H':
+					nbreDeMotsh++;
+					Container.buildPremiereLettreList("h", nbreDeMotsh);
+					break;
+				case 'i': case 'I': case 'î':
+					nbreDeMotsi++;
+					Container.buildPremiereLettreList("i", nbreDeMotsi);
+					break;
+				case 'j': case 'J':
+					nbreDeMotsj++;
+					Container.buildPremiereLettreList("j", nbreDeMotsj);
+					break;
+				case 'k': case 'K':
+					nbreDeMotsk++;
+					Container.buildPremiereLettreList("k", nbreDeMotsk);
+					break;
+				case 'l': case 'L':
+					nbreDeMotsl++;
+					Container.buildPremiereLettreList("l", nbreDeMotsl);
+					break;
+				case 'm': case 'M':
+					nbreDeMotsm++;
+					Container.buildPremiereLettreList("m", nbreDeMotsm);
+					break;
+				case 'n': case 'N':
+					nbreDeMotsn++;
+					Container.buildPremiereLettreList("n", nbreDeMotsn);
+					break;
+				case 'o': case 'O': case 'ô':
+					nbreDeMotso++;
+					Container.buildPremiereLettreList("o", nbreDeMotso);
+					break;
+				case 'p': case 'P':
+					nbreDeMotsp++;
+					Container.buildPremiereLettreList("p", nbreDeMotsp);
+					break;
+				case 'q': case 'Q':
+					nbreDeMotsq++;
+					Container.buildPremiereLettreList("q", nbreDeMotsq);
+					break;
+				case 'r': case 'R':
+					nbreDeMotsr++;
+					Container.buildPremiereLettreList("r", nbreDeMotsr);
+					break;
+				case 's': case 'S':
+					nbreDeMotss++;
+					Container.buildPremiereLettreList("s", nbreDeMotss);
+					break;
+				case 't': case 'T':
+					nbreDeMotst++;
+					Container.buildPremiereLettreList("t", nbreDeMotst);
+					break;
+				case 'u': case 'U':
+					nbreDeMotsu++;
+					Container.buildPremiereLettreList("u", nbreDeMotsu);
+					break;
+				case 'v': case 'V':
+					nbreDeMotsv++;
+					Container.buildPremiereLettreList("v", nbreDeMotsv);
+					break;
+				case 'w': case 'W':
+					nbreDeMotsw++;
+					Container.buildPremiereLettreList("w", nbreDeMotsw);
+					break;
+				case 'x': case 'X':
+					nbreDeMotsx++;
+					Container.buildPremiereLettreList("x", nbreDeMotsx);
+					break;
+				case 'y': case 'Y':
+					nbreDeMotsy++;
+					Container.buildPremiereLettreList("y", nbreDeMotsy);
+					break;
+				case 'z': case 'Z':
+					nbreDeMotsz++;
+					Container.buildPremiereLettreList("z", nbreDeMotsz);
+					break;				
+				default :
+					System.out.println(line.charAt(0));
+					break;
+				}
+
+				//Méthode par switch pour le nombre de lettres
+
+				switch (line.length()) {
+				case 1:
+					nbreDeLettres1++;
+					Container.buildNbreLettresList(1, nbreDeLettres1);
+					break;
+				case 2:
+					nbreDeLettres2++;
+					Container.buildNbreLettresList(2, nbreDeLettres2);
+					break;
+				case 3:
+					nbreDeLettres3++;
+					Container.buildNbreLettresList(3, nbreDeLettres3);
+					break;
+				case 4:
+					nbreDeLettres4++;
+					Container.buildNbreLettresList(4, nbreDeLettres4);
+					break;
+				case 5:
+					nbreDeLettres5++;
+					Container.buildNbreLettresList(5, nbreDeLettres5);
+					break;
+				case 6:
+					nbreDeLettres6++;
+					Container.buildNbreLettresList(6, nbreDeLettres6);
+					break;
+				case 7:
+					nbreDeLettres7++;
+					Container.buildNbreLettresList(7, nbreDeLettres7);
+					break;
+				case 8:
+					nbreDeLettres8++;
+					Container.buildNbreLettresList(8, nbreDeLettres8);
+					break;
+				case 9:
+					nbreDeLettres9++;
+					Container.buildNbreLettresList(9, nbreDeLettres9);
+					break;
+				case 10:
+					nbreDeLettres10++;
+					Container.buildNbreLettresList(10, nbreDeLettres10);
+					break;
+				case 11:
+					nbreDeLettres11++;
+					Container.buildNbreLettresList(11, nbreDeLettres11);
+					break;
+				case 12:
+					nbreDeLettres12++;
+					Container.buildNbreLettresList(12, nbreDeLettres12);
+					break;
+				case 13:
+					nbreDeLettres13++;
+					Container.buildNbreLettresList(13, nbreDeLettres13);
+					break;
+				case 14:
+					nbreDeLettres14++;
+					Container.buildNbreLettresList(14, nbreDeLettres14);
+					break;
+				case 15:
+					nbreDeLettres15++;
+					Container.buildNbreLettresList(15, nbreDeLettres15);
+					break;
+				case 16:
+					nbreDeLettres16++;
+					Container.buildNbreLettresList(16, nbreDeLettres16);
+					break;
+				case 17:
+					nbreDeLettres17++;
+					Container.buildNbreLettresList(17, nbreDeLettres17);
+					break;
+				case 18:
+					nbreDeLettres18++;
+					Container.buildNbreLettresList(18, nbreDeLettres18);
+					break;
+				case 19:
+					nbreDeLettres19++;
+					Container.buildNbreLettresList(19, nbreDeLettres19);
+					break;
+				case 20:
+					nbreDeLettres20++;
+					Container.buildNbreLettresList(20, nbreDeLettres20);
+					break;
+				case 21:
+					nbreDeLettres21++;
+					Container.buildNbreLettresList(21, nbreDeLettres21);
+					break;
+				case 22:
+					nbreDeLettres22++;
+					Container.buildNbreLettresList(22, nbreDeLettres22);
+					break;
+				case 23:
+					nbreDeLettres23++;
+					Container.buildNbreLettresList(23, nbreDeLettres23);
+					break;
+				case 24:
+					nbreDeLettres24++;
+					Container.buildNbreLettresList(24, nbreDeLettres24);
+					break;
+				case 25:
+					nbreDeLettres25++;
+					Container.buildNbreLettresList(25, nbreDeLettres25);
+					break;
+				default:
+					System.err.println(line.length());
+					break;
+				}
+
 			}	
 			if (line.endsWith("ver")) {
-				//				System.out.println(line);
 				Container.builEndVerList(line);
 			}
 
-			/*for (int nbre = 1; nbre <= 50; nbre++) {
-				if (nbre == line.length()) {
-
-				}
-			}*/
-
-			//Méthode par switch
-
-			switch (line.charAt(0)) {
-			case 'a':
-				nbreDeMotsa++;
-				Container.buildPremiereLettreList("a", nbreDeMotsa);
-				break;
-			case 'b':
-				nbreDeMotsb++;
-				Container.buildPremiereLettreList("b", nbreDeMotsb);
-				break;
-			case 'c':
-				nbreDeMotsc++;
-				Container.buildPremiereLettreList("c", nbreDeMotsc);
-				break;
-			case 'd':
-				nbreDeMotsd++;
-				Container.buildPremiereLettreList("d", nbreDeMotsd);
-				break;
-			case 'e':
-				nbreDeMotse++;
-				Container.buildPremiereLettreList("e", nbreDeMotse);
-				break;
-				/*case 'é':
-				nbreDeMotse++;
-				Container.buildPremiereLettreList("e", nbreDeMotse);
-				break;*/
-			case 'f':
-				nbreDeMotsf++;
-				Container.buildPremiereLettreList("f", nbreDeMotsf);
-				break;
-			case 'g':
-				nbreDeMotsg++;
-				Container.buildPremiereLettreList("g", nbreDeMotsg);
-				break;
-			case 'h':
-				nbreDeMotsh++;
-				Container.buildPremiereLettreList("h", nbreDeMotsh);
-				break;
-			case 'i':
-				nbreDeMotsi++;
-				Container.buildPremiereLettreList("i", nbreDeMotsi);
-				break;
-			case 'j':
-				nbreDeMotsj++;
-				Container.buildPremiereLettreList("j", nbreDeMotsj);
-				break;
-			case 'k':
-				nbreDeMotsk++;
-				Container.buildPremiereLettreList("k", nbreDeMotsk);
-				break;
-			case 'l':
-				nbreDeMotsl++;
-				Container.buildPremiereLettreList("l", nbreDeMotsl);
-				break;
-			case 'm':
-				nbreDeMotsm++;
-				Container.buildPremiereLettreList("m", nbreDeMotsm);
-				break;
-			case 'n':
-				nbreDeMotsn++;
-				Container.buildPremiereLettreList("n", nbreDeMotsn);
-				break;
-			case 'o':
-				nbreDeMotso++;
-				Container.buildPremiereLettreList("o", nbreDeMotso);
-				break;
-			case 'p':
-				nbreDeMotsp++;
-				Container.buildPremiereLettreList("p", nbreDeMotsp);
-				break;
-			case 'q':
-				nbreDeMotsq++;
-				Container.buildPremiereLettreList("q", nbreDeMotsq);
-				break;
-			case 'r':
-				nbreDeMotsr++;
-				Container.buildPremiereLettreList("r", nbreDeMotsr);
-				break;
-			case 's':
-				nbreDeMotss++;
-				Container.buildPremiereLettreList("s", nbreDeMotss);
-				break;
-			case 't':
-				nbreDeMotst++;
-				Container.buildPremiereLettreList("t", nbreDeMotst);
-				break;
-			case 'u':
-				nbreDeMotsu++;
-				Container.buildPremiereLettreList("u", nbreDeMotsu);
-				break;
-			case 'v':
-				nbreDeMotsv++;
-				Container.buildPremiereLettreList("v", nbreDeMotsv);
-				break;
-			case 'w':
-				nbreDeMotsw++;
-				Container.buildPremiereLettreList("w", nbreDeMotsw);
-				break;
-			case 'x':
-				nbreDeMotsx++;
-				Container.buildPremiereLettreList("x", nbreDeMotsx);
-				break;
-			case 'y':
-				nbreDeMotsy++;
-				Container.buildPremiereLettreList("y", nbreDeMotsy);
-				break;
-			case 'z':
-				nbreDeMotsz++;
-				Container.buildPremiereLettreList("z", nbreDeMotsz);
-				break;				
-			default :
-			}
-			
 		}
+
+		System.out.println(titre);
 		System.out.println("nombre de mots en : " + Container.listPremiereLettre);
+		System.out.println("longueur des mots : " + Container.listNbreLettres);
 		System.out.println("list recupérée " + Container.listDeMot.size());
 		System.out.println("nbre mot ver" + Container.listVer.size());
 		scan.close();
